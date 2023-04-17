593bd9e732101d316b8c709faa9c2c1689006e9f
anhduynguyen1207 <anhduynguyen1@gmail.com>
2023-04-17 23:21:47 +0700

    Duy add code write_log

--- Chat_GPT.py
+++ Chat_GPT.py
@@ -5,32 +5,52 @@ import time
 from gtts import gTTS
 import openai
 import random
-# Text - to - speech: Khai báo hàm chuyén doi van bán thanh giong nói
+import datetime
+
+# Get the current date and time
+now = datetime.datetime.now()
+
+# Format the date and time as a string
+date_string = now.strftime('%Y-%m-%d_%H-%M-%S')
+
+# Text - to - speak
 
 
 def speak(text):
     print("GPT: {}".format(text))
+    write_log(text)
     tts = gTTS(text=text, lang='en', slow=False)
     tts.save("sound.mp3")
     playsound.playsound("sound.mp3", True)
     os.remove("sound.mp3")
 
-# Speech - to - text: Khai báo hàm chuyén doi giong nói thanh van bán
+# Speech - to - text:
 
 
 def get_text():
-    print("GPT: Dang nghe...")
+    print("GPT: Listening...")
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        #print("Tôi: ", end='')
         audio = r.listen(source, phrase_time_limit=5)
         try:
-            text = r.recognize_google(audio, language="en-En")
+            text = r.recognize_google(audio, language="ev-En")
+            write_log(text)
             print(text)
-            return text.lower
+            return text.lower()
         except:
-            #print("Dang nghe...")
             return 0
+
+
+def write_log(text):
+
+    # Construct the filename using the date and time
+    filename = f"my_file_{date_string}.txt"
+
+    # Open the file for writing
+    with open(filename, 'a') as f:
+        # Write to the file
+        f.write(text)
+        f.write('\n')
 
 
 def get_again():
@@ -48,11 +68,11 @@ def get_again():
 
 
 def chat_gpt():
-    speak("Hello, Iam Chatgpt")
+    speak("Hello, May I help you?")
     question = get_again()
     while True:
         try:
-            openai.api_key = "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"
+            openai.api_key = ""
             model_engine = "text-davinci-003"
             completions = openai.Completion.create(
                 engine=model_engine,
@@ -68,13 +88,15 @@ def chat_gpt():
         except:
             time.sleep(2)
             speak(random.choice([
-                "Hello", "Hi", "Wapsup"
+                "Sorry I can get it.Can you say it again",
+                "I dont get it, Please repeat again!",
+                "What you say, again please"
             ]))
         question = get_again()
-        if "Good bye" in question or "Welcome" in question:
-            speak("See you agian!!")
-        time.sleep(1)
-        break
+        if "goodbye" in question or "welcome" in question:
+            speak("See you again!!")
+            time.sleep(1)
+            break
 
 
 chat_gpt()
---
+++ README.md
@@ -0,0 +1,9 @@
+# install requirement 
+```
+pip install playsound
+pip install SpeechRecognition
+pip install python-time
+pip install gTTS       
+pip install openai  
+pip install random
+```
